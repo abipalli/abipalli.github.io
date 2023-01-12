e4aece35e785bea42be8d852f38c3548d3fec8ca
abdev <>
2023-01-12 00:32:51 -0800

    Update next.config.js with akamai loader config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/dist/shared/lib/constants')
+
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
-    path: ''
+    path: './'
   }
 }
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  return nextConfig
+}
